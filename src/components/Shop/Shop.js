240b76109843592f01ca3c822ac2349a533b4c2d
aktarulrahul <contact.aktarul@gmail.com>
2021-09-23 02:09:17 +0600

    shop layout and data load done

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import './Shop.css';
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://raw.githubusercontent.com/ProgrammingHero1/ema-john-simple-resources/master/fakeData/products.JSON'
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div className="shop-container">
+      <div></div>
+      <div className="product-container">
+        <h3>Products {products.length}</h3>
+      </div>
+      <div className="cart-container">
+        <h3>Order Summary</h3>
+        <h5>Items Order</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
