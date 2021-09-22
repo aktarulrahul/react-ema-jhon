f0100c53c7d577bf590133473b52517367c9a2a5
aktarulrahul <contact.aktarul@gmail.com>
2021-09-23 04:48:24 +0600

    cart calculation added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,12 +6,18 @@ const Cart = ({ cart }) => {
   for (const product of cart) {
     total = total + product.price;
   }
+  const shipping = 15;
+  const tax = (total + shipping) * 0.1;
+  const grandTotal = total + tax + shipping;
   return (
     <div>
       <h3>Order Summary</h3>
       <h5>Items Ordered: {cart.length}</h5>
       <br />
-      <p>Total: {total}</p>
+      <p>Total: {total.toFixed(2)}</p>
+      <p>Shipping: {shipping}</p>
+      <p>Tax: {tax.toFixed(2)}</p>
+      <p>Grand Total: {grandTotal.toFixed(2)}</p>
     </div>
   );
 };
