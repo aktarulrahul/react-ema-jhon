163c560baf8cf25889b3e0d36ac66150b3298848
aktarulrahul <contact.aktarul@gmail.com>
2021-10-09 20:38:11 +0600

    firebase initial setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBqNACo2IywtZ_1itYNWAe5mIBKHB9_B0g',
+  authDomain: 'react-ema-john-4b2de.firebaseapp.com',
+  projectId: 'react-ema-john-4b2de',
+  storageBucket: 'react-ema-john-4b2de.appspot.com',
+  messagingSenderId: '459705466418',
+  appId: '1:459705466418:web:5d9c5cb2a4c4f470de062b',
+};
+
+export default firebaseConfig;
